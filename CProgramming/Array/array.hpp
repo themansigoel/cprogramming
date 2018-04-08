490e12f0fbceffe465898b9af1eb5c0c6b636d1b
Mansi Goel <themansigoel@gmail.com>
2018-04-08 20:03:16 +0530

    new files added

---
+++ CProgramming/Array/array.hpp
@@ -0,0 +1,17 @@
+//
+//  array.hpp
+//  CProgramming
+//
+//  Created by RAHUL GOEL on 04/04/18.
+//
+//
+
+#ifndef array_hpp
+#define array_hpp
+
+#include <stdio.h>
+int arraySum();
+int maxElement();
+int sortArray();
+
+#endif /* array_hpp */
